c38a7a873443c5230417eeb3045c0ef83e695e02
Ivan Ni <ivanni2003@gmail.com>
2024-09-23 23:11:36 -0400

    notes on how to run(see App.jsx for frontend and index.js for backend)

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
+/*
+How to run:
+1) ensure you are in backend directory
+2) type npm run dev in terminal
+3) open localhost 3001 (we don't really use this, it's just to ensure that backend works)
+*/
+
+
 const express = require('express')
 const app = express()
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
+/*
+How to run:
+1) ensure you are in frontend directory
+2) type npm run dev in terminal
+3) open localhost 5173
+
+*/
 function App() {
   return (
     <>

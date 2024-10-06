17f2fa917bcb195f24248d81457849e833e4b52c
DistantJragon <jacobgoodwin2017@gmail.com>
2024-10-06 17:46:46 -0400

    Organized Map.jsx Added popup functionality for licking on buildings

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Circle, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer, CircleMarker, Popup, useMap } from 'react-leaflet';
 
 const baseURL = "http://localhost:3001";
 const buildingAPI = `${baseURL}/api/ways/buildings`;
@@ -15,20 +15,38 @@ const MapViewUpdater = ({ latitude, longitude, zoom }) => {
   }, [latitude, longitude, zoom, map]); 
 };
 
-const Map = ({ latitude, longitude, zoom, buildings }) => {
+const PopupContent = ({ building }) => {
+  return "Popup content here.";
+}
 
+const BuildingsRenderer = ({ buildings }) => {
+  const buildingPathOptions = {
+    color: 'black',
+    fillColor: 'gold',
+  };
+  return buildings.map((building, index) => {
+    return <CircleMarker
+      key={index}
+      center={[building.buildingPosition.lat, building.buildingPosition.lon]}
+      // TODO
+      // radius={building.buildingPosition.rad} // Right now, this makes the radius too small
+      // buildingPosition.rad is in lat/lon, leaflet uses meters for circle radius
+      radius={5}
+      pathOptions={buildingPathOptions}
+      >
+      <Popup><PopupContent building={building} /></Popup>
+    </CircleMarker>
+  });
+}
+
+const Map = ({ latitude, longitude, zoom, buildings }) => {
   return (
     <MapContainer center={[latitude, longitude]} zoom={zoom} style={{ height: "600px", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {
-        // Display the buildings
-        buildings.map((building, index) => {
-          return <Circle key={index} center={[building.buildingPosition.lat, building.buildingPosition.lon]} radius={building.buildingPosition.rad} fillColor="black" fillOpacity={0.5} />;
-        })
-      }
+      <BuildingsRenderer buildings={buildings} />
       <MapViewUpdater latitude={latitude} longitude={longitude} zoom={zoom} /> {/* Include the updater */}
     </MapContainer>
   );

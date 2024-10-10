993c6f2d35d045bf44619b554ad3daf340d15476
Nathaniel <laughablehaha@gmail.com>
2024-10-10 11:17:53 -0400

    Added longitude and latitude to navNode.js as well as some contextual comments.

--- backend/models/navWay.js
+++ backend/models/navWay.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const NavNode = require('./navNode')
 
 const navWaySchema = new mongoose.Schema({
+    // "nodes" is an array of "navNode.js" schema objects.
     nodes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'NavNode' }],
     length: { type: Number },
     type: { type: String, default: 'footpath' },

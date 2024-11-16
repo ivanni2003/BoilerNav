ae7d45ad65be081f30ffc537d14a4d8a296aca3b
Nathaniel <laughablehaha@gmail.com>
2024-11-16 14:14:29 -0500

    Added get-update-requests endpoint

--- backend/controllers/indoorDataInfo.js
+++ backend/controllers/indoorDataInfo.js
@@ -29,7 +29,7 @@ indoorDataRouter.get('/:name', async(request, response) => {  // get by name
     }
 })
 
-// Add API to submit update requests
+// Add endpoint to submit update requests
 indoorDataRouter.post('/update-request', async (request, response) => {
     const { buildingName, roomId, newRoomName, username } = request.body;
     try {
@@ -47,5 +47,22 @@ indoorDataRouter.post('/update-request', async (request, response) => {
       response.status(500).json({ error: 'Failed to submit update request' });
     }
   });
+
+// GET endpoint to get ALL update requests.
+indoorDataRouter.get('/get-update-requests', async (request, response) => {
+  try {
+    const updateRequests = await UpdateRequest.find({})
+        if (!updateRequests) {
+            return response.status(404).send('indoor data not found')
+        }
+        response.json(updateRequests)
+  } catch (error) {
+    console.error('Error handling update request:', error);
+    response.status(500).json({ error: 'Failed to submit update request' });
+  }
+});
+
+// GET endpoint to DECLINE or APPROVE an update request.
+
 
 module.exports = indoorDataRouter
